6c66abd7b720b3274348c5d3ceb8f915913022c0
Jordão Macedo <jordao.macedo@projeto.lead>
2018-07-09 12:57:37 -0300

    More on Redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import {  View } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
 import { Header } from './components/common'
+import LibraryList from './components/LibraryList'
 
 const App = () => {
-    return (
-        <Provider store={createStore(reducers)}>
-            <View>
-                <Header headerText="Home Page" />
-            </View>
-        </Provider>
-    );
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={styles.appStyle}>
+        <Header headerText="Home Page" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
 };
+
+const styles = {
+  appStyle: {
+    flex: 1
+  }
+}
 
 export default App;
--- src/reducers/LibraryReducer.js
+++ src/reducers/LibraryReducer.js
@@ -1,1 +1,3 @@
-export default () => [];
+import data from './data/LibraryList.json'
+
+export default () => data;
